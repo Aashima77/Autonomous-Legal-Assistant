27c582f7c846e4edc1ce7ca10b6930b296913211
Anirudh Jaggi <anirudhjaggi.forte@gmail.com>
2025-07-04 14:19:20 +0530

    fix: update batch size

--- utils/embedder.py
+++ utils/embedder.py
@@ -15,7 +15,7 @@ def mean_pooling(model_output, attention_mask):
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return (token_embeddings * input_mask_expanded).sum(1) / input_mask_expanded.sum(1)
 
-def get_embeddings(texts, tokenizer, model, batch_size=32):
+def get_embeddings(texts, tokenizer, model, batch_size=64):
     embeddings = []
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
@@ -61,35 +61,3 @@ def get_embeddings(texts, tokenizer, model, batch_size=32):
 #             cls_embedding = output.last_hidden_state[:, 0, :].squeeze().numpy()
 #             embeddings.append(cls_embedding)
 #     return embeddings
-
-
-
-# from transformers import AutoTokenizer, AutoModel
-# import torch
-# from tqdm import tqdm
-
-# def load_inlegalbert(model_name="law-ai/InLegalBERT"):
-#     tokenizer = AutoTokenizer.from_pretrained(model_name)
-#     model = AutoModel.from_pretrained(model_name)
-#     model.eval()
-#     return tokenizer, model
-
-# def mean_pooling(model_output, attention_mask):
-#     token_embeddings = model_output.last_hidden_state  # (batch_size, seq_len, hidden_dim)
-#     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-#     return (token_embeddings * input_mask_expanded).sum(1) / input_mask_expanded.sum(1)
-
-# def get_embeddings(texts, tokenizer, model, batch_size=32):
-#     embeddings = []
-#     with torch.no_grad():
-#         for i in tqdm(range(0, len(texts), batch_size)):
-#             batch_texts = texts[i:i+batch_size]
-#             encoded_input = tokenizer(batch_texts, padding=True, truncation=True, max_length=512, return_tensors="pt")
-#             output = model(**encoded_input)
-#             pooled_output = mean_pooling(output, encoded_input['attention_mask'])
-#             embeddings.extend(pooled_output.cpu().numpy())
-#     return embeddings
-
-
-
-
